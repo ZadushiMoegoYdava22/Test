54e730874bafd0dd89ebd9bcb91deee961f805b6
ZadushiMoegoYdava22 <44941387+ZadushiMoegoYdava22@users.noreply.github.com>
2018-11-17 19:34:27 +0300

    Add files via upload

---
+++ modules/admin/views/product/view.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model app\modules\admin\models\Product */
+
+$this->title = $model->name;
+$this->params['breadcrumbs'][] = ['label' => 'Products', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="product-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('Обновление', ['update', 'id' => $model->id], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('Удаление', ['delete', 'id' => $model->id], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => 'Вы действительно хотите удалить данный товар?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'id',
+            'category_id',
+            'name',
+            'content:html',
+            'price',
+            'keywords',
+            'description',
+            'img',
+            'hit',
+            'new',
+            'sale',
+        ],
+    ]) ?>
+
+</div>
